e0958e1b75c08c5b4c90a3eed6cab4c8c04c9a5d
rickrizzle <rickh39@gmail.com>
2019-09-15 21:50:15 -0400

    State, functions, child components; add Plus, Minus, Zero buttons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = props => {
   const [counter, setCounter] = useState(0);
 
-  const increaseByOne = () => setCounter(counter + 1);
-
-  const setToZero = () => setCounter(0);
+  const setToValue = value => setCounter(value);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+      <Button onClick={() => setToValue(counter + 1)} text="plus" />
+      <Button onClick={() => setToValue(counter - 1)} text="minus" />
+      <Button onClick={() => setToValue(0)} text="zero" />
     </div>
   );
 };
