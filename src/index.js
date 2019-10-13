ee181b4be4c37000038b9641a1dd74b5d48a35c6
rickrizzle <rickh39@gmail.com>
2019-10-13 19:47:26 -0400

    Different app components; 1000 button

--- src/index.js
+++ src/index.js
@@ -1,45 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const History = props => {
-  if (props.allClicks.length === 0) {
-    return <div>the app is used by pressing buttons</div>;
-  }
+const Display = props => <div>{props.value}</div>;
 
-  return <div>button press history: {props.allClicks.join("")}</div>;
-}; //Conditional rendering: powerful stuff here.
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Button = props => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = props => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  //When the buttons are clicked, the respective letter(s) are stored in the array allClicks
-  //Push method would also work to add to array BUT don't do this: state of Reach components must NOT be mutated directly. Can lead to problems even if it
-  //doesn't seem to be an issue.
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("Poo"));
-    setLeft(left + 1);
+  const setToValue = newValue => {
+    setValue(newValue);
   };
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat("POO"));
-    setRight(right + 1);
-  };
+  // Do not define components inside another component
 
   return (
     <div>
-      <div>
-        {left}
-        <Button onClick={handleLeftClick} text="left" />
-        <Button onClick={handleRightClick} text="right" />
-        {right}
-        <History allClicks={allClicks} />
-      </div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
-    //"join" method joins all the items into a single string; separated by the string passed as a function paramter, in our case an empty space.
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("root"));
